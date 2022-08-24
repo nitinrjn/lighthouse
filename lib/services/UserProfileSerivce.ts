280832b27fbfb3376ac99b50e6853ddad18efbd9
Nicu Parente <nicuparente@gmail.com>
2022-08-23 21:49:27 -0700

    added CRUD on user api

--- lib/services/UserProfileSerivce.ts
+++ lib/services/UserProfileSerivce.ts
@@ -1,24 +1,34 @@
 import prisma from "../util/prisma";
 import { UserProfile } from "@prisma/client";
 
+class UserProfileResult {
+    userProfile: UserProfile = null;
+    success: boolean = false; 
+    error: string = "";
+}
 
 export const getUserByEmail = async (email:string) =>{
-    const userProfile : UserProfile = await prisma.userProfile.findUnique({
-        where: {
-            email: email
-        }
-    })
-    
-    return userProfile;
+    const result: UserProfileResult = new UserProfileResult();
+
+    try{
+        result.userProfile = await prisma.userProfile.findUnique({
+            where: {
+                email: email
+            }
+        })
+        result.success = result.userProfile != null ? true : false;
+    }
+    catch(e){
+        result.error = e.code
+
+    }
+
+    return result;
 }
 
 export const createUser = async (userProfile: UserProfile) =>{
-    const result = {
-        userProfile: null,
-        success: false,
-        error: null
-    }
-    
+    const result: UserProfileResult = new UserProfileResult();
+
     try{
         result.userProfile = await prisma.userProfile.create({
             data: {...userProfile}
@@ -26,6 +36,43 @@ export const createUser = async (userProfile: UserProfile) =>{
         result.success = result.userProfile != null ? true : false;
     }
     catch(e){
+        console.log(`ERROR encountered PrismaCode=${e.code}`);
+        result.error = e.code
+    }
+    finally{
+        return result;
+    }
+}
+
+export const updateUser = async(userProfile: UserProfile) =>{
+    const result: UserProfileResult = new UserProfileResult();
+
+    try {
+        result.userProfile = await prisma.userProfile.update({
+            where: {
+                email: userProfile.email
+            },
+            data: {...userProfile}
+        })
+    }catch(e){
+        console.log(`ERROR encountered PrismaCode=${e.code}`);
+        result.error = e.code
+    }
+    finally{
+        return result;
+    }
+}
+
+export const deleteUser = async(userProfile: UserProfile) =>{
+    const result: UserProfileResult = new UserProfileResult();
+
+    try {
+        result.userProfile = await prisma.userProfile.delete({
+            where: {
+                email: userProfile.email
+            }
+        })
+    }catch(e){
         console.log(`ERROR encountered PrismaCode=${e.code}`);
         result.error = e.code
     }
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,20 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession, Session, UserProfile } from '@auth0/nextjs-auth0';
 
-import { getUserByEmail, createUser } from "../../lib/services/UserProfileSerivce";
+import { createUser, deleteUser, getUserByEmail , updateUser, UserProfileResult  } from "../../lib/services/UserProfileSerivce";
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
     const session: Session = getSession(req,res);
-
-    if(req.method == "GET"){
-        const userProfile = await getUserByEmail(session.user.email);
-        return res.status(200).json({userProfile: userProfile})
+    
+    if(req.method === "GET"){
+        const result= await getUserByEmail(session.user.email);
+        return res.status(200).json(result)
     }
 
-    if(req.method == "POST"){
+    if(req.method === "POST"){
         const result = await createUser(req.body)
-        console.log(result)
         return res.status(200).json(result);
     }
+
+    if(req.method === "PUT"){
+        const result = await updateUser(req.body)
+        return res.status(200).json(result);
+    }
+
+    if(req.method === "DELETE"){
+        const result = await updateUser(req.body)
+        return res.status(200).json(result);
+
+    }
+
     return res.status(400).json({status: "Invalid Request"});
 }
